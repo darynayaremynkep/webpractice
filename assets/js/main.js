36f8bdc888e6d5f4c746a5fe58af333f0edb75b7
Daryna <yaremyndaryna@icloud.com>
2026-05-31 18:01:45 +0300

    Created a navbar for all screen types

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,15 @@ const iconBlock = document.getElementById('iconBlock');
 const iconOpen = document.getElementById('iconOpen');
 const iconClose = document.getElementById('iconClose');
 const mainMenu = document.getElementById('mainMenu');
+const topBtnLogin = document.getElementById('topBtnLogin');
+const topBtnCreateAccount = document.getElementById('topBtnCreateAccount');
+const logo = document.getElementById('logo');
 
 iconBlock.addEventListener('click', () => {
     iconOpen.classList.toggle('d-none');
     iconClose.classList.toggle('d-none');
     mainMenu.classList.toggle('menu-open');
+    topBtnLogin.classList.toggle('hidden-btn');
+    topBtnCreateAccount.classList.toggle('hidden-btn');
+    logo.classList.toggle('move-up');
 });
